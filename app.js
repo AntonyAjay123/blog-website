71e4ee18322c7b4d8504f50e846b2f111853ba05
Antony Ajay Geoffrey <ajaygeoffrey456@gmail.com>
2022-04-05 14:10:26 +0530

    Add login features to the site. Users can now see only the posts that they have composed

--- app.js
+++ app.js
@@ -5,17 +5,32 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ =require("lodash");
 const mongoose= require("mongoose");
+const session= require("express-session");
+const passport = require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.use(session({
+  secret:"thisIsTheSecretString",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //-----------------Mongoose-------------
 
-mongoose.connect("mongodb+srv://admin-antony:billiejean456@cluster0.bczwr.mongodb.net/blogDatabase",{useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/usersBlog",{useNewUrlParser:true});
 const blogSchema= new mongoose.Schema({
   title:{
     type:String,
@@ -29,14 +44,31 @@ const blogSchema= new mongoose.Schema({
 
 const Post=mongoose.model("Post",blogSchema);
 
+const userSchema=new mongoose.Schema({
+  email:String,
+  password:String,
+  posts:[blogSchema]
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User= mongoose.model("User",userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+
+
 
 // -----------------website----------------
 
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 
 //Home Screen
 app.get("/",function(req,res){
@@ -51,12 +83,27 @@ app.get("/",function(req,res){
     startContent:homeStartingContent,
     posts:posts});
   });*/
-  Post.find({},function(err,data){
-    if(err)console.log(err);
-    else{
-      res.render("home",{title:"Home",startContent:homeStartingContent,posts:data});
-    }
-  });
+  if(req.isAuthenticated()){
+    console.log(req.user);
+    User.findOne({username:req.user.username},function(err,foundUser){
+      if(err)console.log(err);
+      else{
+        res.render("home",{title:"Home",startContent:homeStartingContent,posts:foundUser.posts});
+      }
+    });
+  }
+  else{
+    res.render("home",{title:"Home",startContent:homeStartingContent,posts:[]});
+  }
+    // Post.find({},function(err,data){
+      // if(err)console.log(err);
+      // else{
+        // res.render("home",{title:"Home",startContent:homeStartingContent,posts:data});
+      // }
+    // });
+  // }
+  // else res.render("home",{title:"Home",startContent:homeStartingContent,posts:[]});
+
 });
 
 
@@ -116,10 +163,66 @@ app.post("/compose",function(req,res){
     title:req.body.composeTitle,
     body:req.body.composeBody
   });
+  let currentUser=req.user;
+  User.findOne({username : currentUser.username},function(err,foundUser){
+    if(err)console.log(err);
+    else{
+      foundUser.posts.push(newPost);
+      foundUser.save();
+    }
+  });
   newPost.save(function(err){
     if(!err)
     res.redirect("/");
   });
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+app.post("/register",function(req,res){
+  User.register({ username : req.body.email}, req.body.password, function(err,user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    }
+    else{
+      passport.authenticate("local")(req,res,function(){
+        console.log("done");
+        res.redirect("/");
+      });
+    }
+  });
+});
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+app.post("/login",function(req,res){
+  const user = new User({
+    username: req.body.email,
+    password:req.body.password
+  });
+  req.login(user,function(err){
+    if(err){
+      console.log(err);
+      res.redirect('/login');
+    }
+    else{
+      console.log("reached");
+      passport.authenticate("local")(req,res,function(){
+        console.log("authenticated");
+        res.redirect("/");
+      });
+    }
+  });
+});
+
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
 });
 
 
